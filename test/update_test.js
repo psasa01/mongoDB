ef32e7a2f9e083ae3f674d27acd46d8dd4421ba9
psasa01 <psasa01@gmail.com>
2017-09-18 09:18:37 +0200

    message

--- test/update_test.js
+++ test/update_test.js
@@ -11,15 +11,63 @@ describe('Updating user', () => {
             .then(() => done());
     });
 
-    it('instance set n save', (done) => {
-        joe.set('name', 'Mujo');
-        joe.save()
+    function assertName(operation, done) {
+        operation
             .then(() => User.find({}))
             .then((users) => {
                 assert(users.length === 1);
                 assert(users[0].name === 'Mujo');
                 done();
             });
+    }
+
+    it('instance type using set n save', (done) => {
+        joe.set('name', 'Mujo');
+        assertName(joe.save(), done);
+
     });
 
+    it('model instance can update', (done) => {
+        assertName(joe.update({
+            name: 'Mujo'
+        }), done);
+    });
+
+    it('a model class can update', (done) => {
+        assertName(
+            User.update({
+                name: 'Joe'
+            }, {
+                name: 'Mujo'
+            }),
+            done
+        );
+
+    });
+
+    it('a model class can update one record', (done) => {
+        assertName(
+            User.findOneAndUpdate({
+                name: 'Joe'
+            }, {
+                name: 'Mujo'
+            }),
+            done
+        );
+    });
+
+    it('a model class can find a record with id and update', (done) => {
+        assertName(
+            User.findByIdAndUpdate({
+                _id: joe._id
+            }, {
+                name: 'Mujo'
+            }),
+            done
+        );
+
+    });
 });
+
+
+// tested on 09.12
