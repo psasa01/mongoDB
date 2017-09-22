756d9f17b91b78ef4f466fc216f82a3e940f96ad
psasa01 <psasa01@gmail.com>
2017-09-22 08:51:18 +0200

    220917

--- test/reading_test.js
+++ test/reading_test.js
@@ -2,12 +2,27 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Reading records', () => {
-    let joe;
+    let joe, maria, alex, zach;
     beforeEach((done) => {
+        alex = new User({
+            name: 'Alex'
+        });
         joe = new User({
             name: 'Joe'
         });
-        joe.save()
+        maria = new User({
+            name: 'Maria'
+        });
+        zach = new User({
+            name: 'Zach'
+        });
+
+        Promise.all([
+            maria.save(),
+            alex.save(),
+            joe.save(),
+            zach.save()
+                    ])
             .then(() => {
                 done();
             });
@@ -16,8 +31,6 @@ describe('Reading records', () => {
         User.find({
             name: 'Joe'
         }).then((users) => {
-            console.log(users[0]._id);
-            console.log(joe._id);
             assert(users[0]._id.toString() === joe._id.toString());
             done();
         });
@@ -26,10 +39,23 @@ describe('Reading records', () => {
         User.findOne({
             _id: joe._id
         }).then((user) => {
-            console.log(user);
             assert(user.name === 'Joe');
             done();
         })
+    });
+    it('can skip and limit the result set', (done) => {
+        User.find({})
+            .sort({
+                name: 1
+            })
+            .skip(1)
+            .limit(2)
+            .then((users) => {
+                assert(users.length === 2);
+                assert(users[0].name === 'Joe');
+                assert(users[1].name === 'Maria');
+                done();
+            });
     });
 });
 
